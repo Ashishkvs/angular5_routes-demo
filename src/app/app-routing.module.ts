a190d389f99d387ac96909d42df697cf05d420c8
Ashish Yadhuvanshi <ashishkvs@gmail.com>
2018-04-19 18:33:40 +0530

    wildcard routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DepartmentListComponent } from './department-list/department-list.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  { path: 'departments', component: DepartmentListComponent },
-    { path: 'employees', component: EmployeeListComponent }
+ // { path: '', component: DepartmentListComponent},  // for deafult page load
+ // OR
+ { path: '', redirectTo: '/departments' , pathMatch: 'full'},  // or sometime pathMatch: 'prefix' also used BUT Not in this case
+ { path: 'departments', component: DepartmentListComponent },
+  { path: 'employees', component: EmployeeListComponent },
+  { path: '**', component: PageNotFoundComponent} // it shud be decalredat the last of routes other wise it will work for all urls
 ];
 
 @NgModule({
@@ -13,3 +18,8 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponent = [
+  DepartmentListComponent,
+  EmployeeListComponent,
+  PageNotFoundComponent
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponent } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { EmployeeListComponent } from './employee-list/employee-list.component';
-import { DepartmentListComponent } from './department-list/department-list.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeListComponent,
-    DepartmentListComponent
+    routingComponent
   ],
   imports: [
     BrowserModule,
